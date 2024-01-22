78f7b62b896400cefd109677a47e79dcbf7b360b
AaradhayaMehta <151171374+AaradhayaMehta@users.noreply.github.com>
2024-01-22 18:04:49 +0530

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,31 @@
+var y= Math.floor(Math.random()* 10+1);
+playername=localStorage.getItem("playername");
+var guess= 1
+function submit()
+{
+var x= document.getElementById("guessField").value;
+if(x==y)
+{
+alert("CONGRATULATIONS!!!"+playername+"U GUESSED IT RIGHT IN"+ guess + "GUESS");
+guess= 1;
+}
+else if(x>y)
+{
+    guess++;
+    alert("OOPS SORRY!! TRY A SAMLLER NUMBER");
+}
+else
+{
+guess++;
+alert("OOPS SORRY! TRY A LARGER NUMBER");
+}
+}
+function playAgain()
+{
+    y= Math.floor(Math.random()* 10+1);
+}
+
+// count of attempts
+// until hit
+  
+// function for the number sent by the user
---
+++ login.js
@@ -0,0 +1,7 @@
+function Login()
+{
+playername = document.getElementById("playername").value;
+localStorage.setItem("playername", playername);
+window.location= "gamepage.html"
+}
+
